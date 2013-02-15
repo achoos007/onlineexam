c2c613594160abb9d2dd23b9d38825890f8d3f9b
Optiplex <Optiplex@GENIUS4>
2013-02-15 16:49:16 +0530

    Location based user list updated

--- application/views/assigneelist.php
+++ application/views/assigneelist.php
@@ -1,7 +1,41 @@
 <?php
 if ($assigneeid == '1') {
-    $query = $this->db->query("select staff_id,first_name,last_name from tbl_staffs where status='Active' and country='231' group by first_name asc");
-    $ind = $this->db->query("select first_name,last_name from tbl_staffs where status='Active' and country='101' group by first_name asc");
+    // for getting country code
+    $co['table'] = 'country';
+    $co = getrecords($co);
+
+
+    $bhr['table'] = 'tbl_staffs';
+    $bhr['where']['status'] = 'Active';
+    $bhr['where']['country'] = 'BHR';
+    $bhr['order']['first_name'] = 'asc';
+    $bhr = getrecords($bhr);
+
+    $ind['table'] = 'tbl_staffs';
+    $ind['where']['status'] = 'Active';
+    $ind['where']['country'] = 'IND';
+    $ind['order']['first_name'] = 'asc';
+    $ind = getrecords($ind);
+
+    $kwt['table'] = 'tbl_staffs';
+    $kwt['where']['status'] = 'Active';
+    $kwt['where']['country'] = 'KWT';
+    $kwt['order']['first_name'] = 'asc';
+    $kwt = getrecords($kwt);
+
+    $omn['table'] = 'tbl_staffs';
+    $omn['where']['status'] = 'Active';
+    $omn['where']['country'] = 'OMN';
+    $omn['order']['first_name'] = 'asc';
+    $omn = getrecords($omn);
+
+    $are['table'] = 'tbl_staffs';
+    $are['where']['status'] = 'Active';
+    $are['where']['country'] = 'ARE';
+    $are['order']['first_name'] = 'asc';
+    $are = getrecords($are);
+
+
 
 // For getting title name
     $qdes['table'] = 'qdesigner';
@@ -10,66 +44,78 @@ if ($assigneeid == '1') {
     $title = $qdes['title'];
     ?>
 
-
+<div id="sucess-msg"></div>
     <div data-role="content" >
         <div class="content-primary">
             <ul data-role="listview" data-inset='true' data-filter='true'>
-                <li data-role="list-divider">Bhaharin</li>
-
-
-
-
                 <?php
-                if ($query->num_rows() > 0) {
-
-                    foreach ($query->result() as $row) {
-                        print"<li id='ques_" . $row->staff_id . "' ><a href=''> $row->first_name $row->last_name";
-                        print"</a><a href='#deleteQuestion'  data-icon='add'  data-theme='d'  class='assign_user'>Delete</a></li>";
+                foreach ($co['result'] as $row) {
+                    $code = $row['code'];
+                    $cname = $row['name'];
+                    ?>
+                    <li data-role="list-divider"><?php print $cname; ?></li>
+                    <?php
+                    if ($code == 'BHR') {
+                        foreach ($bhr['result'] as $row) {
+                            $str = ucfirst(strtolower($row['first_name'])) . "&nbsp;" . ucfirst(strtolower($row['last_name']));
+                            print"<li class='bhr' id='".$row['staff_id']."' ><a  href=''>" . $str ."</a></li>";
+                            
+                        }
+                    }
+                    if ($code == 'IND') {
+                        foreach ($ind['result'] as $row) {
+                            $str = ucfirst(strtolower($row['first_name'])) . "&nbsp;" . ucfirst(strtolower($row['last_name']));
+                            print"<li class='bhr' id='" . $row['staff_id'] . "'><a href=''>" . $str."</a></li>";
+                            
+                        }
+                    }
+                    if ($code == 'KWT') {
+                        foreach ($kwt['result'] as $row) {
+                            $str = ucfirst(strtolower($row['first_name'])) . "&nbsp;" . ucfirst(strtolower($row['last_name']));
+                            print"<li><a href=''>" . $str."</a></li>";
+                            
+                        }
+                    }
+                    if ($code == 'OMN') {
+                        foreach ($omn['result'] as $row) {
+                            $str = ucfirst(strtolower($row['first_name'])) . "&nbsp;" . ucfirst(strtolower($row['last_name']));
+                            print"<li class='bhr' id='" . $row['staff_id'] . "'><a href=''>" . $str."</a></li>";
+                            
+                        }
+                    }
+                    if ($code == 'ARE') {
+                        foreach ($are['result'] as $row) {
+                            $str = ucfirst(strtolower($row['first_name'])) . "&nbsp;" . ucfirst(strtolower($row['last_name']));
+                            print"<li class='bhr' id='" . $row['staff_id'] . "'><a href=''>" . $str ."</a></li>";
+                            
+                        }
                     }
                 }
-                ?>		
-
-                <li data-role="list-divider">India</li>
-                <?php
-                if ($ind->num_rows() > 0) {
-
-                    foreach ($ind->result() as $row) {
-                        print"<li><a href=''> $row->first_name $row->last_name";
-                        print"</a><a href='#deleteQuestion'  data-icon='add'  data-theme='d' class='assign_user'>Delete</a></li>";
-                    }
-                }
-                ?>	
-
-                <li data-role="list-divider">Kuwait</li>
-                <li><a href="index.html">Caleb Booth</a></li>
-                <li><a href="index.html">Christopher Adams</a></li>
-                <li><a href="index.html">Culver James</a></li>
-                <li data-role="list-divider">Oman</li>        
-                <li data-role="list-divider">United Arab Emirates</li>
+                ?>
             </ul>
         </div>
     </div>
     <div id="deleteQuestion" data-role='popup' style='width:250px; padding:50px; border:5px solid #B9B9B9;' data-theme='d'>
         You have added this user(s) to <?php echo $title; ?> exam
         <div class='clear'><br/><br/></div>
-            <?php
-            print button('Assign', '', 'delete_question');
-            print close();
-        } else {
-            print"No record found";
-        }
-        ?>
+    <?php
+    print button('Assign', '', 'delete_question');
+    print close();
+} else {
+    print"No record found";
+}
+?>
 </div>
         <?php
         $script = "
     
-$('.assign_user').click(function(){
-clickId=this.id;
-$( '#deleteQuestion' ).popup( 'open' );
-
-//alert('Hai');
-$('#ques_'+clickId+'').hide(2000);
-});
-    ";
+                $('.bhr').click(function(){
+                clickId=this.id;
+                $.post('".site_url('exam/user_selection/')."',{clkid:clickId,qid:$qid},function(data){
+                     $('#sucess-msg').html(data);
+                });
+                   
+                });
+        ";
         print ready($script);
         ?>
